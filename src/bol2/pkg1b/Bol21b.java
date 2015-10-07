ab6f02e7f5baab914c7103c373f39dd416a404c1
plagoromani <plagoromani@atanasoff09.danielcastelao.org>
2015-10-07 10:36:47 +0200

    Final

---
+++ src/bol2/pkg1b/Bol21b.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bol2.pkg1b;
+
+/**
+ *
+ * @author plagoromani
+ */
+public class Bol21b {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        float base = 2.0f ;
+        float altura = (float)3.0 ;
+                System.out.println("area="+ base*altura/2);
+    }
+    
+}
